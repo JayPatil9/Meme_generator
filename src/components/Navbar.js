d6de0cf278487f82d455febd0661d1f35544a48a
Real-404 <157211421+Real-404@users.noreply.github.com>
2024-01-20 17:03:34 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Header() {
     <nav className="nav">
       <img className="logo" src={logo} alt="logo" />
       <h3 className="nav-h3">ReactFacts</h3>
-      <h4 className="nav-h4">by JAY</h4>
+      <h4 className="nav-h4">by Team_Real</h4>
     </nav>
     </header>
   )
