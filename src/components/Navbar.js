1025660872323864b65dc156679bdee075a26797
jaypatil2415@gmail.com <jaypatil2415@gmail.com>
2024-01-27 10:53:01 +0530

    update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,9 @@ import logo from '../logo.svg'
 export default function Header() {
   return (
     <header className="App">
-    <nav className="nav">
       <img className="logo" src={logo} alt="logo" />
       <h2 className="nav-h2">Meme Generator</h2>
       <h4 className="nav-h4">by JAY</h4>
-    </nav>
     </header>
   )
 }
