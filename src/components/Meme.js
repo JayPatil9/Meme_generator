2ecebf3e9cecd53fdeb2c4fa29c2bd710cf5e16a
JayPatil9 <75982633+JayPatil9@users.noreply.github.com>
2024-01-28 15:04:46 +0530

    Update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,28 +2,29 @@ import React from 'react'
 import memesData from '../memesData.js'
 
 
-const [meme,setMeme] = React.useState({
+export default function Meme() {
+
+  const [meme,setMeme] = React.useState({
   topText: "",
   bottomText: "",
   randomImage: ""
-})
+  })
 
-const [allMemesData,setAllMemesData] = React.useState(memesData)
+  const [allMemesData,setAllMemesData] = React.useState(memesData)
 
 
-function getMemeImage() {
-  const memesArray = allMemesData.data.memes
-  const randomNumber = Math.floor(Math.random() + memesArray.length)
-  const url = memesArray[randomNumber].url
-  setMeme(prevMeme => ({
-    ...prevMeme,
-    randomImage: url
-  }))
+  function getMemeImage() {
+    const memesArray = allMemesData.data.memes
+    const randomNumber = Math.floor(Math.random() + memesArray.length)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        randomImage: url
+    }))
   
-}
+  }
 
-
-export default function Meme() {
+  
   return (
   <div className="main">
     <div className="form">
